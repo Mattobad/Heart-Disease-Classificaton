fcb75b2a079b6e89fd829ca1bcd43fef9c21a53d
Mattobad <sarbottamsinjali@gmail.com>
2021-03-05 19:48:05 -0400

    update workflow for deployment

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from app.app import  create_app
 from app.config import config
 
+app = create_app(config_object=config.DevelopmentConfig)
+
 
 if __name__ == '__main__':
-    app = create_app(config_object=config.DevelopmentConfig)
     app.run(debug=True)
